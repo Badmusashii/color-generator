946c38891732405adf0c73a324e894e90f505320
KILINC Bayram <badmusashii@gmail.com>
2023-01-24 12:56:49 +0100

    Essaie2

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const r = Math.floor(Math.random * 255);
+const r = Math.floor(Math.random() * 255);
+const g = Math.floor(Math.random() * 255);
+const b = Math.floor(Math.random() * 255);
 
-console.log(r);
+const color = `rgb(${r}, ${g}, ${b})`;
+
+document.body.style.background = color;
+document.body.innerHTML = `<h1>${color}</h1>`;
