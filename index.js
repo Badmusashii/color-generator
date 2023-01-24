ff4e73faa6674c8eff0cc0636b767f58ac49676c
KILINC Bayram <badmusashii@gmail.com>
2023-01-24 12:51:06 +0100

    nouvelle essaie

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-function getcolor() {
-  const r = Math.floor(Math.random() * 255);
-  const g = Math.floor(Math.random() * 255);
-  const b = Math.floor(Math.random() * 255);
+const r = Math.floor(Math.random * 255);
 
-  const color = `rgb(${r}, ${g}, ${b})`;
-
-  document.body.style.background = color;
-
-  document.body.innerHTML = `<h2>${color}</h2>`;
-}
-
-setInterval(getcolor, 2000);
+console.log(r);
